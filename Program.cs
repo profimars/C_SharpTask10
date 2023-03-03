f88f6184c713034797f630d66e3b5314814cfc7f
Марс <profimars@mail.ru>
2023-03-03 14:06:50 +0300

    Добавил задачу 10

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача 10
+//Напишите програамму, которая на вход принимает трехзначное число, и на выходе  
+//показывает вторую цифру этого числа. Через строку решать нельзя.
+//756 -> 5
+//982 -> 8
+//918 -> 1
+//https://github.com/profimars/C_SharpTask10
+Console.WriteLine("Ведите трехзначное число");
+int q = Convert.ToInt32(Console.ReadLine());
+int q12 = q / 10;
+Console.WriteLine("Первые две цифры трехзначного числа = " + q12);
+int q2 = q12 % 10;
+Console.WriteLine("вторая цифра трехзначного числа = " + q2);
